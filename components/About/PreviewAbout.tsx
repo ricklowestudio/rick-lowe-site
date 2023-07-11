a86a0a3390111b7e1389cb81a38244b422c0934d
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2023-07-11 10:22:34 -0500

    fix(about): fixes type for about props to preview about

--- components/About/PreviewAbout.tsx
+++ components/About/PreviewAbout.tsx
@@ -5,7 +5,11 @@ import { useLiveQuery } from "@sanity/preview-kit";
 import About from "./";
 import { aboutQuery } from "@/sanity/lib/queries";
 
-export default function PreviewAbout({ about }: { about: SanityDocument }) {
+export default function PreviewAbout({
+	about = [],
+}: {
+	about: SanityDocument[];
+}) {
 	const [data] = useLiveQuery(about, aboutQuery);
 
 	return <About about={data} />;
