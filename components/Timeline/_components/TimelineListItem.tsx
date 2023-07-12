131b18f9a12df5afe6c9379e7d98c0a24fe1293a
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2023-07-12 18:04:56 -0500

    refactor(timeline): changes font weight and size
    Changes font weight on the startPeriod and endPeriod, as well as changes font size on title in
    `<TimelineListItem />`

--- components/Timeline/_components/TimelineListItem.tsx
+++ components/Timeline/_components/TimelineListItem.tsx
@@ -9,18 +9,18 @@ function TimelineListItem({
 		<li className="mb-12 ml-4 px-6 py-3">
 			<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
 			{startPeriod ? (
-				<time className="mb-1 text-lg font-normal leading-none text-black">
+				<time className="mb-1 text-lg font-bold leading-none text-black">
 					{startPeriod}{" "}
 				</time>
 			) : null}
 			{endPeriod ? (
-				<time className="mb-1 text-lg font-normal leading-none text-black">
+				<time className="mb-1 text-lg font-bold leading-none text-black">
 					- {endPeriod}
 				</time>
 			) : null}
 			{image ? <ImageComponent value={image} /> : null}
 
-			<h3 className="text-base font-normal text-black">{title}</h3>
+			<h3 className="text-lg font-normal text-black">{title}</h3>
 			<BlockContent value={description} />
 
 			{videoUrl ? <VideoPlayer url={videoUrl} /> : null}
